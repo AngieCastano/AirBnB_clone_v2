aabec2510749d3238b7aa34e84e64f57dbb81169
AngieCastano1634 <1634@holbertonschool.com>
2020-08-18 04:09:34 +0000

    [task 1]

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,8 @@ from fabric.operations import local
 
 def do_pack():
     """generates the comppressed archive"""
-    File_name = "versions/web_static_{}.tgz".format(
+    local("mkdir -p versions")
+    File_name = "web_static_{}.tgz".format(
         datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
     result = local("tar -zcvf versions/{} web_static".format(File_name))
 
