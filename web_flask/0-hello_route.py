fed145f0d236a02dde5b871410e89077968ef2a6
AngieCastano1634 <1634@holbertonschool.com>
2020-09-01 17:39:54 -0500

    [app.run() when not imported]

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,13 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.run(host='0.0.0.0')
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
         """Print hello hbnb"""
-        print("Hello HBNB!")
         return "Hello HBNB!"
+
+if __name__ == '__main__':
+        ''' when not imported'''
+        app.run(host='0.0.0.0', port=5000)
