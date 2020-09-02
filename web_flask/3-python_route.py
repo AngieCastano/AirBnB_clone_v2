2827ff83d38e90c6638b7289f439d4741918c00b
AngieCastano1634 <1634@holbertonschool.com>
2020-09-01 21:39:04 -0500

    [python/text]

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This displays “Hello HBNB!”"""
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+        """Print hello hbnb"""
+        return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+        """Print hbnb"""
+        return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+            """Show c and text"""
+            return 'C %s' % escape(text).replace("_", " ")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+        """Show Python is cool[default]"""
+        return 'Python %s' % escape(text).replace("_", " ")
+
+
+if __name__ == '__main__':
+        ''' when not imported'''
+        app.run(host='0.0.0.0', port=5000)
