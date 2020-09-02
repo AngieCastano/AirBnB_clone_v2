1fc821ef6f8af15524b4e3dcefc7201332447012
AngieCastano1634 <1634@holbertonschool.com>
2020-09-01 21:41:18 -0500

    [/python/(<text>)]

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_is_fun(text):
             return 'C %s' % escape(text).replace("_", " ")
 
 
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/(<text>)', strict_slashes=False)
 def python_is_cool(text="is cool"):
         """Show Python is cool[default]"""
         return 'Python %s' % escape(text).replace("_", " ")
