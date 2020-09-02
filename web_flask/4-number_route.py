f0ddc3a3852a1d709eeb29361f1b360946cb2dc3
AngieCastano1634 <1634@holbertonschool.com>
2020-09-01 21:58:08 -0500

    [n is an integer]

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This displays “Hello HBNB!”"""
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+        """Print hello hbnb"""
+        return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+        """Print hbnb"""
+        return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+            """Show c and text"""
+            return 'C %s' % escape(text).replace("_", " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+        """Show Python is cool[default]"""
+        return 'Python %s' % escape(text).replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_integer(n):
+        """displays only if n is an integer"""
+        return '%s is a number' % escape(n)
+
+
+if __name__ == '__main__':
+        ''' when not imported'''
+        app.run(host='0.0.0.0', port=5000)
